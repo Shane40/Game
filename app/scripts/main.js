a704ad57999b5ec0463e0c24556a8374f2176b7f
Shane Blatt <shane.blatt@yahoo.com>
2015-02-05 14:57:49 -0500

    some font changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
 console.log('The Iron Yard Rocks');
+console.log("This is the scary game");
+
+
+var player1 = $('#player1box'),
+    moveBTN = $('#player1Move');
+
+
+//Imported 
+    document.getElementById("blattMath").innerHTML = ((Math.random() * 10) + 1);
+
+
+
+function greetings() {
+       alert("X");
+      };
