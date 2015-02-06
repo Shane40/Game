79100de05b12fd4e2036d083f94d745187fc372b
Shane Blatt <shane.blatt@yahoo.com>
2015-02-05 20:33:15 -0500

    fumbling about with JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,24 +2,22 @@ console.log('The Iron Yard Rocks');
 console.log("This is the scary game");
 
 
-// var player1 = $('#player1box'),
-//     moveBTN = $('#player1Move');
+var c = document.getElementById("Canvas1");
+var ctx = c.getContext("2d");
+ctx.font = "60px Raleway, sans-serif";
+ctx.strokeText("X",10,50);
 
+//Function that pops up the Blatt names at the push of a button
+function greetings() {
+       alert("Mike, Bindy, Shane, Chance and Tara");
+      }
 
-// //Imported 
-//     document.getElementById("blattMath").innerHTML = ((Math.random() * 10) + 1);
-
-
-
-// function greetings() {
-//        alert("Mike, Bindy, Shane, Chance and Tara");
-//       };
-
-
-//   $('.story-tab').on('click', function(){
-// 	$('.story-content').addClass('story-active');
-// });
-
-function canvasClicked(canvasNumber)
-
-	
+function myFunction() {
+    document.getElementById("move1").innerHTML = ('X');
+}
+function myFunction() {
+    document.getElementById("move2").innerHTML = ('X');
+}
+function myFunction() {
+    document.getElementById("move3").innerHTML = ('X');
+}
